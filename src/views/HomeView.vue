b4a05e72a55086b3af7588f2a79747a64687e802
Nikola M <nikola.merlic2@gmail.com>
2022-03-24 18:17:36 +0100

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  import axios from 'axios';
+  import HelloWorld from '../components/HelloWorld'
+  import func from 'vue-editor-bridge';
+
+
+  export default {
+    name: 'HomevieW',
+    data: function() {
+      return {
+        comitovi: [],
+      };
+      
+    },
+    
+
+  async mounted(){
+
+  let agify = await axios.get("https://api.agify.io?name=Nikola")
+                              .then(response => this.totalVuePackages
+                               = response.data.total);
+
+  let genderize = await axios.get("https://api.genderize.io?name=Nikola")
+                                .then(response => this.totalVuePackages
+                                = response.data.total);
+  
+  let nationalize = await axios.get("https://api.nationalize.io?name=Nikola")
+                                .then(response => this.totalVuePackages
+                                = response.data.total);
+
+  let gotov1 = await response1.json();
+  let gotov2 = await response2.json();
+  let gotov3 = await response3.json();
+
+  this.comitovi = gotov1;
+  this.comitovi = gotov2;
+   this.comitovi = gotov3;
+
+
+
+  console.log(agify);
+  console.log(genderize);
+  console.log(nationalize);
+
+
+
+  
+
